cc1c5dcb7040742e59734ca00f1aafce73860faf
Luis Pereira <luispereira.tkd@gmail.com>
2022-08-31 14:56:01 +0100

    added play_the_book with tags

--- play_the_book.sh
+++ play_the_book.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-ansible-playbook -K run.yml
+if [ "$#" -gt 0 ]; then
+  # run with tags
+  # the first tag will be duplicated, because the script its easier
+  TAGS="$1"
+  for TAG in "${@:2}"; do
+    TAGS=$TAGS",$TAG"
+  done
+
+  ansible-playbook -K --tags $TAGS run.yml
+
+else
+  ansible-playbook -K run.yml
+fi
+
